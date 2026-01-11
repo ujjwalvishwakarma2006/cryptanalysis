58cc3df23a3bdce855901cf5b4d8923443f21b17
ujjwal@archlinux <2023ucs0116@iitjammu.ac.in>
2026-01-12 02:58:50 +0530

    Modified and generalized affine cipher.

--- affine-cipher/main.cpp
+++ affine-cipher/main.cpp
@@ -1,16 +1,39 @@
 // Code to decrypt affine cipher. 
 
 #include <bits/stdc++.h>
-
+using namespace std;
 using pii = std::pair<int, int>;
+
+struct mapping {
+public:
+    char from, to;
+
+    mapping(char _from, char _to) {
+        from = _from;
+        to = _to;
+    }
+};
+
+struct points {
+public:
+    mapping first, second;
+    
+    // points(mapping _first, mapping _second) {
+    //     first = _first;
+    //     second = _second;
+    // }
+
+    points(char c1, char p1, char c2, char p2) : first(p1, c1), second(p2, c2) {} 
+
+    void print() {
+        cout << first.from << "->" << first.to << 
+            "\t" << second.from << "->" << second.to
+            << "\n";
+    }
+};
 
 inline void print_pair(pii p) {
     std::cout << "(" << p.first << ", " << p.second << ")\n";
-}
-
-inline bool print_inverse_error(int x, int n) {
-    std::cout << "Inverse of " << x << " does not exist in Z_" << n << ".\n";
-    return false;
 }
 
 // Function to find inverse of x in Zn
@@ -25,26 +48,42 @@ inline int find_inverse(int x, int n) {
 // ax1 + b = r1 mod n and ax2 + b = r2 mod n
 // where a, b, x1, x2, r1, r2 belongs to Zn
 // Specifically a belongs to Zn*
-inline pii find_a_b(int x1, int r1, int x2, int r2, int n) {
+inline pii find_a_b(points map, int n) {
+    
+    int x1 = map.first.from - 'a';
+    int x2 = map.second.from - 'a';
+    int r1 = map.first.to - 'A';
+    int r2 = map.second.to - 'A';
     int x = ((((x1-x2)%n) + n)%n);
     int inverse = find_inverse(x, n);
+
     // Ensure inverse exists
-    // assert(inverse != -1);
-    assert(inverse != -1 || print_inverse_error(x, n));
+    if (inverse == -1) {
+        // std::cout << "Failed guess!!!\n";
+        return {-1, -1}; 
+    }
+
     int a = (((((r1-r2)%n) + n)%n) * inverse)%n;
     int b = (((r1 - a*x1)%n) + n)%n;
+
     // Check if inverse of a exists in Zn or not, because we need that while decryption. 
     int a_inverse = find_inverse(a, n);
-    assert(a_inverse != -1);
-    return {a, b}; 
+
+    if (a_inverse == -1) {
+        // std::cout << "Failed guess!!!\n";
+        return {-1, -1}; 
+    }
+
+    return {a, b};
 }
 
-void run_modulo_tests() {
-    auto result = find_a_b(2, 14, 7, 13, 26);
-    print_pair(result);
-}
+// void run_modulo_tests() {
+//     auto result = find_a_b(2, 14, 7, 13, 26);
+//     print_pair(result);
+// }
 
-void decrypt_and_print(std::string ciphertext, pii a_b, int n) {
+void decrypt_and_print(std::string& ciphertext, pii a_b, int n) {
+
     // Check if a inverse exist or not. 
     int a = a_b.first;
     int b = a_b.second;
@@ -82,57 +121,30 @@ int main() {
 
     // Frequency analysis (assuming a -> 0 and z -> 25)
 
-    pii a_b;
-    
-    // First guess (fails): suppose Z(25) is encryption of e(4) and R(17) is of t(19)
-    // Plug in these values in the two equations. 
-    // a_b = find_a_b(4, 25, 19, 17, 26);
-    
-    // Second guess (fails): suppose Z(25) is encryption of e(4) and R(17) is of a(0)
-    // Plug in these values in the two equations. 
-    // a_b = find_a_b(4, 25, 0, 17, 26);
-    
-    // Second guess (fails): suppose Z(25) is encryption of e(4) and R(17) is of o(14)
-    // Plug in these values in the two equations. 
-    // a_b = find_a_b(4, 25, 14, 17, 26);
-    
-    // Second guess (fails): suppose Z(25) is encryption of t(19) and R(17) is of e(5)
-    // Plug in these values in the two equations. 
-    // a_b = find_a_b(19, 25, 5, 17, 26);
-    
-    // Second guess (fails): suppose Z(25) is encryption of t(19) and R(17) is of a(0)
-    // Plug in these values in the two equations. 
-    // a_b = find_a_b(19, 25, 0, 17, 26);
-    
-    // Second guess (fails): suppose Z(25) is encryption of t(19) and R(17) is of o(14)
-    // Plug in these values in the two equations. 
-    // a_b = find_a_b(19, 25, 14, 17, 26);
-    
-    // Second guess (fails): suppose Z(25) is encryption of a(0) and R(17) is of e(4)
-    // Plug in these values in the two equations. 
-    // a_b = find_a_b(0, 25, 4, 17, 26);
-    
-    // Second guess (fails): suppose Z(25) is encryption of a(0) and R(17) is of t(19)
-    // Plug in these values in the two equations. 
-    // a_b = find_a_b(0, 25, 19, 17, 26);
-    
-    // Second guess (fails): suppose Z(25) is encryption of a(0) and R(17) is of o(14)
-    // Plug in these values in the two equations. 
-    // a_b = find_a_b(0, 25, 14, 17, 26);
-    
-    // Second guess (fails): suppose Z(25) is encryption of o(14) and R(17) is of e(4)
-    // Plug in these values in the two equations. 
-    // a_b = find_a_b(14, 25, 4, 17, 26);
-    
-    // Second guess (fails): suppose Z(25) is encryption of o(14) and R(17) is of t(19)
-    // Plug in these values in the two equations. 
-    // a_b = find_a_b(14, 25, 19, 17, 26);
-    
-    // Second guess (fails): suppose Z(25) is encryption of o(14) and R(17) is of a(0)
-    // Plug in these values in the two equations. 
-    a_b = find_a_b(14, 25, 0, 17, 26);
-    
-    print_pair(a_b);
-    decrypt_and_print(ciphertext, a_b, 26);
+    char most_frequent[4] = {'e', 't', 'a', 'o'};
+    char most_frequent_ct[4] = {'Z', 'R', 'I', 'M'};
+    int n = 26;
+
+    for (char c1 : most_frequent_ct) {
+        for (char c2 : most_frequent_ct) {
+            for (char p1 : most_frequent) {
+                for (char p2 : most_frequent) {
+                    if (c1 == c2) continue;
+                    if (p1 == p2) continue;
+                    points pts(c1, p1, c2, p2); 
+                    auto a_b = find_a_b(pts, n);
+                    if (a_b != make_pair(-1, -1)) {
+                        pts.print();
+                        print_pair(a_b);
+                        decrypt_and_print(ciphertext, a_b, n);
+                    }
+                    else {
+                        cout << "\tFAILED GUESS!!!\n";
+                    }
+                }
+            }
+        }
+    }
+
     return 0;
 }

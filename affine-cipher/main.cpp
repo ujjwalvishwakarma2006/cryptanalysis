d25bc532640c17891ec65c230fea0edb0622a439
ujjwal@archlinux <2023ucs0116@iitjammu.ac.in>
2026-01-15 23:22:16 +0530

    Refactored Affine Cipher code. Improve Readability and Error Handling

--- affine-cipher/main.cpp
+++ affine-cipher/main.cpp
@@ -9,6 +9,8 @@ struct mapping {
 public:
     char from, to;
 
+	mapping () {}
+
     mapping(char _from, char _to) {
         from = _from;
         to = _to;
@@ -18,20 +20,23 @@ public:
 struct points {
 public:
     mapping first, second;
-    
+
+	points() {}
+
     points(char c1, char p1, char c2, char p2) : first(p1, c1), second(p2, c2) {} 
 
     void print() {
-        cout << first.from << "->" << first.to << 
+		std::cout << first.from << "->" << first.to << 
             "\t" << second.from << "->" << second.to
             << "\n";
     }
 };
 
 class Group {
-	int n;
+	int n = 26;
 public:
 
+	Group() {}
 	Group(int _n) : n(_n) {}
 
 	// Function to find inverse of x in Zn
@@ -52,22 +57,21 @@ public:
 		int r1 = map.first.to - 'A';
 		int r2 = map.second.to - 'A';
 		int x = ((((x1-x2)%n) + n)%n);
-		int inverse = find_inverse(x, n);
+		int x_inverse = inverse(x);
 
 		// Ensure inverse exists
-		if (inverse == -1) {
+		if (x_inverse == -1) {
 			// std::cout << "Failed guess!!!\n";
 			return {-1, -1}; 
 		}
 
-		int a = (((((r1-r2)%n) + n)%n) * inverse)%n;
+		int a = (((((r1-r2)%n) + n)%n) * x_inverse)%n;
 		int b = (((r1 - a*x1)%n) + n)%n;
 
-		// Check if inverse of a exists in Zn or not, because we need that while decryption. 
-		int a_inverse = find_inverse(a, n);
+		// Check if inverse of a keyExistsin Zn or not, because we need that while decryption. 
+		int a_inverse = inverse(a);
 
 		if (a_inverse == -1) {
-			// std::cout << "Failed guess!!!\n";
 			return {-1, -1}; 
 		}
 
@@ -79,7 +83,7 @@ class AffineCipher {
 	char encrypt_char(char ch) {
 		if (ch > 'z' || ch < 'a') {
 			std::cout << "Unable to encrypt character: " << ch << "\n\tExpects only lowercase english alphabets\n";
-			return "";
+			return '0';
 		}	
 
 		if (!key.exists()) {
@@ -97,7 +101,7 @@ class AffineCipher {
 	char decrypt_char(char ch) {
 		if (ch > 'Z' || ch < 'A') {
 			std::cout << "Unable to decrypt character: " << ch << "\n\tExpects only lowercase english alphabets\n";
-			return "";
+			return '0';
 		}
 
 		if (!key.exists()) {
@@ -114,6 +118,7 @@ class AffineCipher {
 
 public:
 	struct Key {
+		Group* G = new Group();
 		int a;
 		int b;
 		int a_inv;
@@ -125,32 +130,31 @@ public:
 		}
 		
 		bool exists() {
-			return exists;
+			return keyExists;
 		}
 
 		bool set(int _a, int _b) {
-			int _a_inv = findInverse(_a, n);
+			int _a_inv = G->inverse(_a);
 
 			if (_a_inv == -1) {
-				std::cout << "Cannot set key.a = " << _a << "\n\tInverse won't exist in Z_26\n";
+				std::cout << "\tCannot set key.a = " << _a << "\n\tInverse won't exist in Z_26\n";
 				return false;
 			}
 
 			a = _a;
 			b = _b;
 			a_inv = _a_inv;
-			exists = true;
+			keyExists= true;
 			return true;
 		}
 
 		private:
-			bool exists = false;
-	}
+			bool keyExists= false;
+	};
 	
 	Key key;
-	Group G;
 
-	AffineCipher(_n) : G(Group(_n)) {}
+	AffineCipher() {}
 
 	std::string encrypt(std::string& plaintext) {
 		std::string encrypted;
@@ -167,11 +171,11 @@ public:
 
 	std::string decrypt(std::string& ciphertext) {
 		std::string decrypted;
-		decrypted.reserve(cipertext.length());
+		decrypted.reserve(ciphertext.length());
 
 		for (char ch : ciphertext) {
 			char ch_decrypted = decrypt_char(ch);
-			if (ch_decrypted = '0') return "";
+			if (ch_decrypted == '0') return "";
 			decrypted.push_back(ch_decrypted);
 		}
 
@@ -181,8 +185,8 @@ public:
 	bool deduceKey(std::string plaintext, std::string ciphertext) {
 		int string_length = plaintext.length();
 
-		if (ciphertext.length() != string.length()) {
-			cout << "Can't deduce the key. Lenghts of plaintext and ciphertext are different.\n";
+		if (ciphertext.length() != plaintext.length()) {
+			std::cout << "Can't deduce the key. Lenghts of plaintext and ciphertext are different.\n";
 			return false;
 		}
 
@@ -191,7 +195,7 @@ public:
 		for (int i = 1; i < plaintext.length(); ++i) {
 			if (plaintext[i] == plaintext[i - 1]) continue;
 			keysFound = true;
-			pts = points(ciphertext[i], plaintext[i], ciphertext[i - 1], plaintext[i - 1]);
+			pts = points(ciphertext[i] - 'A', plaintext[i] - 'a', ciphertext[i - 1] - 'A', plaintext[i - 1] - 'a');
 		}
 
 		if (!keysFound) {
@@ -199,9 +203,9 @@ public:
 			return false;
 		}
 
-		auto p = find_a_b(pts, n);
+		auto p = key.G->find_a_b(pts);
 		if (key.set(p.first, p.second) == false) {
-			std::cout << "Can't deduce the key. Inverse doesn't exist.\n";
+			// std::cout << "\tCan't deduce the key. Inverse doesn't exist.\n";
 			return false;
 		}
 
@@ -209,19 +213,15 @@ public:
 	}
 };
 
-inline void print_pair(pii p) {
-    std::cout << "(" << p.first << ", " << p.second << ")\n";
-}
-
 int main() {
+	AffineCipher af;
+
     // ciphertext need to be decrypted.
     std::string ciphertext = "HYVFMRIGJXHMZBTRIZVZUZMHYOZKRURIRFUXFKHMRIGJZIRDQMFDZXXZX";
     std::unordered_map<char, int> char_freq;
     
     for (char c : ciphertext) char_freq[c]++;
-    for (auto [letter, count] : char_freq) {
-        std::cout << letter << ": " << count << "\n";
-    }
+    for (auto [letter, count] : char_freq) std::cout << letter << ": " << count << "\n";
     // Highest frequencies
     // Z: 8
     // R: 7
@@ -239,16 +239,12 @@ int main() {
             for (char p1 : most_frequent) {
                 for (char p2 : most_frequent) {
                     if (p1 == p2) continue;
-                    points pts(c1, p1, c2, p2); 
-                    auto a_b = find_a_b(pts, n);
-                    if (a_b != make_pair(-1, -1)) {
-                        pts.print();
-                        print_pair(a_b);
-                        decrypt_and_print(ciphertext, a_b, n);
-                    }
-                    else {
-                        cout << "\tFAILED GUESS!!!\n";
-                    }
+					std::string from_plaintext = std::string(1, p1) + p2;
+					std::string to_ciphertext = std::string(1, c1) + c2;
+					std::cout << from_plaintext << " -> " << to_ciphertext << "\n";
+					if (!af.deduceKey(from_plaintext, to_ciphertext)) continue;
+					std::string decrypted = af.decrypt(ciphertext);
+					std::cout << decrypted << "\n"; 
                 }
             }
         }
